efd9af5a494393700bd04984ca9828b90324694d
Misterix_dev <emanuelegze87@hotmail.it>
2022-04-26 19:08:40 +0200

    update error afk.lua in cmd
    hi i am yesterday's boy this and the push to change the error

--- server/afk.lua
+++ server/afk.lua
@@ -3,7 +3,7 @@ RegisterNetEvent('KickForAFK', function()
 	DropPlayer(src, 'You Have Been Kicked For Being AFK')
 end)
 
-ESX.RegisterServerCallback(('esx-afkkick:server:GetPermissions', function(source, cb)
+ESX.RegisterServerCallback(('esx_afkkick:server:GetPermissions'), function(source, cb)
     local src = source
     local group = ESX.GetPlayerFromId(src).group
     cb(group)
